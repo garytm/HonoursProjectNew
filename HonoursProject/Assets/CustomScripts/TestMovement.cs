215ea325e9306c6506e9b13ac7c399f79f8d880f
gary <garytmulhall>
2019-02-27 13:50:05 +0000

    VRTK working

---
+++ HonoursProject/Assets/CustomScripts/TestMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour
+{
+    public float speed = 0.1f;
+    public Rigidbody rigidBody;
+	
+	void Update ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3 (moveVertical, 0.0f, moveHorizontal);
+
+        rigidBody.AddForce(movement * speed);
+	}
+}
